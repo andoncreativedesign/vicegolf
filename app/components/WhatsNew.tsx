4ae1d67c19c6347ad534b498887d9a87dee0a635
Your Name <your.email@example.com>
2025-10-31 14:02:53 +0530

    add whats new

---
+++ app/components/WhatsNew.tsx
@@ -0,0 +1,14 @@
+// app/components/WhatsNew.tsx
+export function WhatsNew() {
+  return (
+    <div className="w-full text-center py-10 bg-gray-50 text-gray-900">
+      <h2 className="text-2xl font-semibold mb-3 tracking-wide uppercase">
+        What's New
+      </h2>
+      <p className="text-lg font-medium">
+        uufyfufoufiyfoiyfioyfififiofougiu
+      </p>
+    </div>
+  );
+}
+    
