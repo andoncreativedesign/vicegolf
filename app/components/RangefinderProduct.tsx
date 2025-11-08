9ba7b09949035d2ebcdcb95df92593d99effca1c
Arun Kb <aurn11.kb@gmail.com>
2025-11-08 10:26:48 +0530

    fix: RangefinderProduct video issue

--- app/components/RangefinderProduct.tsx
+++ app/components/RangefinderProduct.tsx
@@ -2,25 +2,31 @@ import type { ProductFragment } from 'storefrontapi.generated';
 import { ProductDetailContents } from '~/components/Product/ProductDetailContents';
 import { Youtube } from '~/components/Youtube';
 import type { ProductDetails } from '~/lib/sanity/products';
+import { VideoSection } from './Product/VideoSection';
+import { useEffect } from 'react';
 
 type RangefinderProductProps = {
   productDetails: ProductDetails | null;
 };
 
 export function RangefinderProduct({ productDetails }: RangefinderProductProps) {
+
+  useEffect(() => {
+    console.log('productDetails ', productDetails)
+  }, [productDetails])
+
   return (
     <>
+      {productDetails?.videoContent && (
+        <VideoSection videoContent={productDetails.videoContent} />
+      )}
+
       {/* Reusable What's New Section */}
       {productDetails && (
         <ProductDetailContents
           content={productDetails?.productContent1?.content || []}
           content2={productDetails?.productContent2?.sections?.[0]}
         />
-      )}
-      
-      {/* Youtube Video Section */}
-      {productDetails?.youtubeVideos && (
-        <Youtube youtubeVideo={productDetails.youtubeVideos} />
       )}
     </>
   );
