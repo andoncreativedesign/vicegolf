f7de3826ff794f8d836185df36133f4718d34503
Your Name <your.email@example.com>
2025-12-06 10:56:16 +0530

    country code fixes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -62,67 +62,15 @@ const transformCollectionsToDropdown = (collections: any[]): ProductDropdownItem
   return dropdowns;
 };
 
-// Inline SVG flags for SSR compatibility
-const flagSvgs: Record<string, string> = {
-  US: `<svg width="20" height="15" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 7410 3900">
-  <path d="M0,0h7410v3900H0" fill="#b31942"/>
-  <path d="M0,450H7410m0,600H0m0,600H7410m0,600H0m0,600H7410m0,600H0" stroke="#FFF" stroke-width="300"/>
-  <path d="M0,0h2964v2100H0" fill="#0a3161"/>
-  <g fill="#FFF">
-    <g id="s18">
-      <g id="s9">
-        <g id="s5">
-          <g id="s4">
-            <path id="s" d="M247,90 317.534230,307.082039 132.873218,172.917961H361.126782L176.465770,307.082039z"/>
-            <use xlink:href="#s" y="420"/>
-            <use xlink:href="#s" y="840"/>
-            <use xlink:href="#s" y="1260"/>
-          </g>
-          <use xlink:href="#s4" x="247" y="210"/>
-        </g>
-        <use xlink:href="#s9" x="494"/>
-      </g>
-      <use xlink:href="#s18" x="988"/>
-      <use xlink:href="#s9" x="1976"/>
-      <use xlink:href="#s5" x="2470"/>
-    </g>
-  </g>
-</svg>`,
-  CA: `<svg width="20" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9600 4800">
-	<path fill="#f00" d="m0 0h2400l99 99h4602l99-99h2400v4800h-2400l-99-99h4602l-99 99H0z"/>
-	<path fill="#fff" d="m2400 0h4800v4800h-4800zm2490 4430-45-863a95 95 0 0 1 111-98l859 151-116-320a65 65 0 0 1 20-73l941-762-212-99a65 65 0 0 1-34-79l186-572-542 115a65 65 0 0 1-73-38l-105-247-423 454a65 65 0 0 1-111-57l204-1052-327 189a65 65 0 0 1-91-27l-332-652-332 652a65 65 0 0 1-91 27l-327-189 204 1052a65 65 0 0 1-111 57l-423-454-105 247a65 65 0 0 1-73 38l-542-115 186 572a65 65 0 0 1-34 79l-212 99 941 762a65 65 0 0 1 20 73l-116 320 859-151a95 95 0 0 1 111 98l-45 863z"/>
-</svg>`,
-  DE: `<svg width="20" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 18">
-	<rect width="30" height="18" y="0" x="0" fill="#000"/>
-	<rect width="30" height="12" y="6" x="0" fill="#D00"/>
-	<rect width="30" height="3" y="12" x="0" fill="#FFCE00"/>
-</svg>`,
-  CH: `<svg width="20" height="15" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-  <path d="m0 0h32v32h-32z" fill="#f00"/>
-  <path d="m13 6h6v7h7v6h-7v7h-6v-7h-7v-6h7z" fill="#fff"/>
-</svg>`,
-  GB: `<svg width="20" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 18">
-<clipPath id="s">
-	<path d="M0,0 v18 h30 v-18 z"/>
-</clipPath>
-<clipPath id="t">
-	<path d="M15,9 h15 v9 z v9 h-15 z h-15 v-9 z v-9 h15 z"/>
-</clipPath>
-<g clip-path="url(#s)">
-	<path d="M0,0 v18 h30 v-18 z" fill="#012169"/>
-	<path d="M0,0 L30,18 M30,0 L0,18" stroke="#fff" stroke-width="4"/>
-	<path d="M0,0 L30,18 M30,0 L0,18" clip-path="url(#t)" stroke="#C8102E" stroke-width="2.6666666666666665"/>
-	<path d="M15,0 v18 M0,9 h30" stroke="#fff" stroke-width="6.666666666666667"/>
-	<path d="M15,0 v18 M0,9 h30" stroke="#C8102E" stroke-width="4"/>
-</g>
-</svg>`,
-  SE: `<svg width="20" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 20">
-  <rect width="32" height="20" fill="#00267a"/>
-  <rect x="8" width="4" height="20" fill="#fedc11"/>
-  <rect y="6" width="32" height="4" fill="#fedc11"/>
-</svg>`,
+// Country flag URLs
+const flagUrls: Record<string, string> = {
+  US: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/united_states.svg?v=1708075042&width=40&height=40&crop=center',
+  CA: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/canada.svg?v=1708075041&width=40&height=40&crop=center',
+  DE: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/germany.svg?v=1708075042&width=40&height=40&crop=center',
+  CH: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/switzerland.svg?v=1708075042&width=40&height=40&crop=center',
+  GB: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/united_kingdom.svg?v=1708075042&width=40&height=40&crop=center',
+  SE: 'https://cdn.shopify.com/s/files/1/0832/9235/6897/files/sweden.svg?v=1712925748&width=40&height=40&crop=center'
 };
-
 
 // Then update the Header component
 export function Header({
@@ -288,7 +236,6 @@ const countries = [
   { code: 'CH', name: 'Switzerland', currency: 'CHF', symbol: 'Fr.', url: 'https://www.vicegolf.ch' },
   { code: 'GB', name: 'United Kingdom', currency: 'GBP', symbol: '£', url: 'https://www.vicegolf.co.uk' },
   { code: 'SE', name: 'Sweden', currency: 'SEK', symbol: 'kr', url: 'https://www.vicegolf.se' },
-  { code: 'AT', name: 'Austria', currency: 'EUR', symbol: '€', url: 'https://www.vicegolf.at' }, // Added Austria
 ] as const;
 
 function AccountToggle({ isLoggedIn }: { isLoggedIn: Promise<boolean> }) {
@@ -359,9 +306,10 @@ function CountryCurrencySelector() {
         onClick={() => setIsOpen(!isOpen)}
         aria-label="Select country and currency"
       >
-        <div
-          className="w-5 h-4 flex items-center justify-center flex-shrink-0"
-          dangerouslySetInnerHTML={{ __html: flagSvgs[selectedCountry.code] }}
+        <img
+          src={flagUrls[selectedCountry.code]}
+          alt={selectedCountry.name}
+          className="w-5 h-4 object-cover flex-shrink-0"
         />
         <span className="hidden sm:inline">
           {selectedCountry.name} ({selectedCountry.currency} {selectedCountry.symbol})
@@ -386,36 +334,25 @@ function CountryCurrencySelector() {
           <div className="fixed inset-0 z-20" onClick={() => setIsOpen(false)} />
 
           {/* Dropdown Content */}
-          <div className="fixed top-[var(--header-height, 80px)] left-0 w-64 bg-white border border-gray-200 rounded-md shadow-lg z-[9999] max-h-[calc(100vh-var(--header-height,80px))] overflow-y-auto">
+          <div className="fixed top-[var(--header-height, 80px)] left-4 w-64 bg-white border border-gray-200 rounded-md shadow-lg z-[9999] max-h-[calc(100vh-var(--header-height,80px))] overflow-y-auto px-4">
             <div className="py-2">
               {filteredCountries.map((country) => (
                 <button
                   key={country.code}
-                  className={`w-full flex items-center space-x-3 px-4 py-3 text-sm hover:bg-gray-50 transition-colors duration-200 ${selectedCountry.code === country.code
+                  className={`w-full flex items-center space-x-3 px-4 py-2 text-sm hover:bg-gray-50 transition-colors duration-200 ${selectedCountry.code === country.code
                     ? 'bg-gray-100 text-gray-900 font-medium'
                     : 'text-gray-700'
                     }`}
                   onClick={() => handleCountrySelect(country)}
                 >
-                  <div
-                    className="w-5 h-4 flex items-center justify-center flex-shrink-0"
-                    dangerouslySetInnerHTML={{ __html: flagSvgs[country.code] }}
+                  <img
+                    src={flagUrls[country.code]}
+                    alt={country.name}
+                    className="w-5 h-4 object-cover flex-shrink-0"
                   />
-                  <div className="flex-1 text-left">
-                    <div className="font-medium">{country.name}</div>
-                    <div className="text-xs text-gray-500">
-                      {country.currency} {country.symbol}
-                    </div>
+                  <div className="flex-1 text-left ml-1 text-sm">
+                    {country.name} ({country.currency} {country.symbol})
                   </div>
-                  {selectedCountry.code === country.code && (
-                    <svg className="w-4 h-4 text-green-600" fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  )}
                 </button>
               ))}
             </div>
