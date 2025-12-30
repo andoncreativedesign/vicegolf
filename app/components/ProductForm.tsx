befea7ff4ec65995a29fb13d5c61166791699726
Madhuchandranp <167729249+Madhuchandranp@users.noreply.github.com>
2025-12-30 16:48:26 +0530

    refact

--- app/components/ProductForm.tsx
+++ app/components/ProductForm.tsx
@@ -91,7 +91,7 @@ export const ProductForm = forwardRef<HTMLDivElement, {
     console.log('product type from details', productType)
   }, [productType]);
   const getProductCustomizationStyleType = (productType: string | undefined): 'drivers' | 'club' | 'default' => {
-  const DRIVER_TYPES = ['drivers', 'hybrids', 'fairway woods','mallet putter','Blade Putter','center mallet putter'];
+  const DRIVER_TYPES = ['drivers', 'hybrids', 'fairway woods','mallet putter','blade putter','center mallet putter'];
 const CLUB_TYPES = ['golf club set', 'golf clubs', 'wedges','irons','drivers'];
 
 const type = productType?.toLowerCase();
