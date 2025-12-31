0b4b779828aadb50d9de99b34d70b24358654494
Your Name <your.email@example.com>
2025-12-31 10:13:47 +0530

    fix text space

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -148,16 +148,29 @@ export function HeroSection({
       {/* Content Layer */}
       <div className={`absolute inset-0 z-20 flex px-[clamp(1rem,4vw,3rem)] ${textColor} ${center ? 'items-center justify-start' : 'items-end justify-center md:justify-start pb-12 md:pb-6'}`}>
         <div className={`max-w-2xl lg:max-w-4xl xl:max-w-5xl py-10 md:py-14 ${bgColor} ${center ? '' : 'text-center md:text-left'}`} style={{ width: '90vw', maxWidth: '1200px' }}>
-          <h1
+          {/* <h1
             className="!text-3xl md:!text-4xl lg:!text-5xl xl:!text-5xl uppercase tracking-tight leading-none mb-1 font-normal"
             style={{ color: current.titleColor }}
           >
             {current.title}
-          </h1>
+          </h1> */}
+
+
+
+  {current.title && (
+            <h2
+              className="!text-3xl md:!text-4xl lg:!text-5xl xl:!text-5xl uppercase tracking-tight leading-none mb-1 font-normal"
+              style={{ color: current.titleColor }}
+            >
+              {current.title}
+            </h2>
+          )}
+
+
 
           {current.text2?.text && (
             <h2
-              className="!text-2xl md:!text-3xl lg:!text-4xl xl:!text-4xl font-extrabold uppercase tracking-tight leading-none mb-4"
+              className="!text-3xl md:!text-4xl lg:!text-5xl xl:!text-5xl uppercase tracking-tight leading-none mb-1 font-normal"
               style={{ color: current.text2.color }}
             >
               {current.text2.text}
