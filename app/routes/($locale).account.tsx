17014b07071d440b4b415d708e3609db75b9d56c
Arun Kb <aurn11.kb@gmail.com>
2026-01-19 17:07:10 +0530

    refact: get discount query refactored

--- app/routes/($locale).account.tsx
+++ app/routes/($locale).account.tsx
@@ -6,8 +6,9 @@ import {
   useLoaderData,
 } from 'react-router';
 import type { Route } from './+types/account';
-import { CUSTOMER_DETAILS_QUERY } from '~/graphql/customer-account/CustomerDetailsQuery';
+import { CUSTOMER_DETAILS_QUERY, GET_DISCOUNT_QUERY } from '~/graphql/customer-account/CustomerDetailsQuery';
 import { SquareUserIcon, HouseIcon, Package2Icon, HomeIcon, LogOutIcon } from 'lucide-react'
+import { axiosShopifyAdmin } from '~/utils/axiosInsatances';
 
 export function shouldRevalidate() {
   return true;
@@ -20,6 +21,20 @@ export async function loader({ context }: Route.LoaderArgs) {
       language: customerAccount.i18n.language,
     },
   });
+
+  const discountCode = 'plus-member-discount-code';
+  const discountData = await axiosShopifyAdmin.post('', {
+    query: GET_DISCOUNT_QUERY,
+    variables: {
+      query: `title:'${discountCode}'`
+    }
+  })
+
+  console.log('\n\n discount response ')
+  console.log(discountData.data)
+  console.log(JSON.stringify(discountData.data))
+  console.log('\n\n discount response end')
+  
 
   if (errors?.length || !data?.customer) {
     throw new Error('Customer not found');
