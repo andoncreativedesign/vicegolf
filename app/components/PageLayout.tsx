00f986cd3335ea1bc284cd8ad303197982708bc1
sunaj <mernsunaj@gmail.com>
2026-02-23 15:58:11 +0530

    comment out chat bot from pagelayout

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -12,7 +12,7 @@ import HeaderMenu from './Header/HeaderMenu';
 import { CartMain } from '~/components/CartMain';
 import { SEARCH_ENDPOINT, SearchFormPredictive, } from '~/components/SearchFormPredictive';
 import { SearchResultsPredictive } from '~/components/SearchResultsPredictive';
-import { ChatbotButton, ChatbotPopup } from './Chatbot/ChatbotButton';
+// import { ChatbotButton, ChatbotPopup } from './Chatbot/ChatbotButton';
 import type { MenuData } from '~/lib/shopify/product-queries';
 import type { Collection } from '@shopify/hydrogen/storefront-api-types';
 import type { BannerData } from '~/lib/sanity/home';
@@ -36,7 +36,7 @@ export function PageLayout({
   publicStoreDomain,
   banner,
 }: PageLayoutProps) {
-  const [isChatOpen, setIsChatOpen] = useState(false);
+  // const [isChatOpen, setIsChatOpen] = useState(false);
 
   return (
     <Aside.Provider>
@@ -54,8 +54,8 @@ export function PageLayout({
         </div>
       </main>
       <Footer footer={footer} header={header} publicStoreDomain={publicStoreDomain} />
-      <ChatbotButton isOpen={isChatOpen} onToggle={() => setIsChatOpen(!isChatOpen)} />
-      <ChatbotPopup isOpen={isChatOpen} onClose={() => setIsChatOpen(false)} />
+      {/* <ChatbotButton isOpen={isChatOpen} onToggle={() => setIsChatOpen(!isChatOpen)} /> */}
+      {/* <ChatbotPopup isOpen={isChatOpen} onClose={() => setIsChatOpen(false)} /> */}
     </Aside.Provider>
   );
 }
